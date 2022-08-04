50ffc7a039e1a65916a148ff2d7ed72465c661ad
Inkmptnz <adrian.peikert3@gmail.com>
2022-08-04 21:55:59 +0200

    add matrix_good_ev inplace funktion

--- linear_algebra.jl
+++ linear_algebra.jl
@@ -2,21 +2,51 @@
 using LinearAlgebra
 using AbstractAlgebra
 
-function matrix_good_ew(dim, range)
-    evs = zeros(n)
-    evs[1] = 0.1 #ensure to run while at least 1
+function matrix_good_ev(dim, possible_values)
+    evs = zeros(dim)
     A = zeros(dim,dim)
-    while !good_ew(evs)
-        A = rand(range[1]:range[2],dim,dim)
+    while !good_ev(evs)
+        A = rand(possible_values,dim,dim)
         evs = eigvals(A)
     end
     return A
 end
 
-function good_ew(evs)
-    if isa(evs,Vector{ComplexF64})
-        return false
+function matrix_good_ev(dim, possible_values)
+    mat = rand(dim, dim)
+    matrix_good_ev!(mat, possible_values)
+    mat
+end
+
+function matrix_good_ev!(mat::AbstractArray, possible_values)
+    evs = zeros(size(mat, 1))
+    evcache = similar(mat)
+    evs[1] = 0.1 #ensure to run while at least 1
+    while !good_ev(evs)
+        evs = random_try!(mat, evcache, possible_values)
     end
+    mat
+end
+
+function random_try!(mat, evcache, possible_values)
+    myrand!(mat, possible_values)
+    copyto!(evcache, mat)
+    eigvals!(evcache)
+end
+
+function myrand!(mat, possible_values)
+    for i in eachindex(mat)
+        mat[i] = rand(possible_values)
+    end
+end
+
+
+
+matrix_good_ev(dim, start, stop) = matrix_good_ev(dim, start:stop)
+
+good_ev(::Vector{ComplexF64}) = false
+
+function good_ev(evs)
     for ev in evs
         if ev ≠ floor(ev)
             return false
@@ -24,6 +54,8 @@ function good_ew(evs)
     end
     return true
 end
+
+matrix_good_ev(2, 1, 5)
 
 
 function char_poly(A) 
@@ -36,7 +68,7 @@ function find_many_good_matrices(count,dim,range)
     good_matrices = fill(zeros(dim,dim),count)
     index = 1
     for i in 1:count
-        good_matrix = matrix_good_ew(dim,range)
+        good_matrix = matrix_good_ev(dim,range)
         if good_matrix ∉ good_matrices
             good_matrices[index] = good_matrix
             index += 1
@@ -56,10 +88,10 @@ function number_good_matrices(matrices)
     end
     return count
 end
-count = 10000
+count = 100000
 dim = 4
 range = (0,1)
-#A = matrix_good_ew(dim, (-3,3))
+#A = matrix_good_ev(dim, (-3,3))
 matrices = find_many_good_matrices(count,dim,range)
 #println(matrices)
 println(number_good_matrices(matrices))
